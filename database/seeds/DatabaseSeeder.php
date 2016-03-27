70b7423c9683c858384d82ac05ef049c24f771aa
elvinoza <elvinoza@gmail.com>
2016-03-27 08:34:15 +0300

    Database settings

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+use App\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +13,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        Model::unguard();
+
+        DB::table('users')->delete();
+
+        $users = array(
+            ['name' => 'name1', 'email' => 'name1@gmail.com', 'password' => Hash::make('secret')],
+            ['name' => 'name2', 'email' => 'name2@gmail.com', 'password' => Hash::make('secret')],
+            ['name' => 'name3', 'email' => 'name3@gmail.com', 'password' => Hash::make('secret')],
+            ['name' => 'name4', 'email' => 'name4@gmail.com', 'password' => Hash::make('secret')],
+        );
+
+        // Loop through each user above and create the record for them in the database
+        foreach ($users as $user)
+        {
+            User::create($user);
+        }
+
+        Model::reguard();
     }
 }
