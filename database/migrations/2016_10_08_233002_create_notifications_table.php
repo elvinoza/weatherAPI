b1936d70d8cf4b1f66e41cd04f5d58b2dba4ba87
elvinoza <elvinoza@gmail.com>
2016-10-09 00:06:10 +0300

    Create notifications table

---
+++ database/migrations/2016_10_08_233002_create_notifications_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNotificationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->integer('disease_model_id');
+            $table->string('short_message');
+            $table->string('full_message');
+            $table->boolean('is_Viewed');
+            $table->boolean('is_read');
+            $table->boolean('is_Valid');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('notifications');
+    }
+}
