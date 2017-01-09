174cf6b1ee55ab4691815662837dc91097fc378a
elvinoza <elvinoza@gmail.com>
2017-01-09 21:27:24 +0200

    added is_admin and is_blocked to users table

---
+++ database/migrations/2017_01_09_212429_add_is_admin_to_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddIsAdminToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->boolean('is_admin')->default(false)->after('password');
+            $table->boolean('is_blocked')->default(false)->after('is_admin');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('is_admin');
+            $table->dropColumn('is_blocked');
+        });
+    }
+}
