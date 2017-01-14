9552270f1088f44c6af3fb97a2ae918553da57ef
elvinoza <elvinoza@gmail.com>
2017-01-15 01:09:59 +0200

    notifications settings table

---
+++ database/migrations/2017_01_15_010156_create_notifications_settings_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNotificationsSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notifications_settings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('station_id');
+            $table->integer('clsf_weather_parameter');
+            $table->integer('compare_operator');
+            $table->boolean('is_active');
+            $table->boolean('is_valid');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('notifications_settings');
+    }
+}
